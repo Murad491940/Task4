dd250e03cf9ed122952c301ca85aeb3639659060
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-06-02 23:29:04 +0400

    homework4

---
+++ MathHelper/MathHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MathHelper
+{
+    internal class MathHelper
+    {
+        public static int PritValue(int a,int b)
+        {
+            return a + b;
+        }
+        public static int PritValue(bool a)
+        {
+
+            if (a == true) ; return 1;
+            if (a == false)  return 0;
+
+            
+        }
+        public static void PritValue(string name,string surname)
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+        public static int PritValue(int a, int b,int c)
+        {
+            return a * b * c;
+        }
+
+    }
+}
---
+++ MathHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MathHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathhelper=new MathHelper();
+            
+            Console.WriteLine(MathHelper.PritValue(5,10));
+            Console.WriteLine(MathHelper.PritValue(true));
+            Console.WriteLine(MathHelper.PritValue(2,3,6));
+            MathHelper.PritValue("Murad","Azizzade");
+
+           
+
+        }
+    }
+}
